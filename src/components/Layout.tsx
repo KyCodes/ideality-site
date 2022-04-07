c05e07d5165aac7f77ea762f52746041f5a817a2
KyCodes <kyler@ky.codes>
2022-04-07 02:53:21 -0600

    fixes: html lang

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,12 @@ export default function Layout(props: layoutProps) {
   return (
     <Fragment>
       <Helmet>
+        <html lang="en" />
         <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Ideality is the ideal way to plan." />
+        <meta name="keywords" content="App, Health, Productivity" />
+        <meta name="author" content="Kyler Fullerton" />
         <title>Ideality | {props.title}</title>
       </Helmet>
       <Header />
